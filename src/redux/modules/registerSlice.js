68a6788401c15d749da18c1344c5eca3c86e36b5
seokuuu <114736988+seokuuu@users.noreply.github.com>
2022-11-15 15:54:14 +0900

    test

--- src/redux/modules/registerSlice.js
+++ src/redux/modules/registerSlice.js
@@ -1,25 +1,17 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
-<<<<<<< HEAD
-export const __register = createAsyncThunk(
-  "register",
-  async (payload, thunkAPI) => {
-    try {
-      await axios.post("http://3.39.193.27:8080/api/member/signup");
-=======
-const BASE_URL = process.env.REACT_APP_SERVER
+const BASE_URL = process.env.REACT_APP_SERVER;
 
 export const __registerUser = createAsyncThunk(
   "regitser",
   async (payload, thunkAPI) => {
     try {
       const response = await axios.post(`${BASE_URL}/members/signup`, payload, {
-        'Content-Type' : 'application/json',
+        "Content-Type": "application/json",
       });
       console.log(payload);
       console.log(response.data);
->>>>>>> 81e37ec9527984565b5aee9861f5980c58285a8c
       return thunkAPI.fulfillWithValue(payload);
     } catch (error) {
       return thunkAPI.rejectWithValue(error);
@@ -47,8 +39,8 @@ export const registerSlice = createSlice({
       .addCase(__registerUser.rejected, (state, action) => {
         state.isLoading = false;
         state.error = action.payload;
-      })
-  }
+      });
+  },
 });
 
 export const { register } = registerSlice.actions;
