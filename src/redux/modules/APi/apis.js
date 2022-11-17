b07f1fbfc37993fbf4e4a89780db861e55f0695c
Sunghoman <ksh_1035@naver.com>
2022-11-17 22:22:50 +0900

    duplicated api delete

--- src/redux/modules/APi/apis.js
+++ src/redux/modules/APi/apis.js
@@ -42,9 +42,6 @@ export const apis = {
   // boards
   getboards: () => api.get(`${BASE_URL}/boards`),
 
-  // boards
-  getboards: () => api.get(`${BASE_URL}/boards`),
-
   // addCreateSlice
   addCreate: (payload) =>
     axios.post(`${BASE_URL}/boards`, payload, {
