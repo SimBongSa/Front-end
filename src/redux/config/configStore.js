a8780dbdbf1a508387d0170713299ac3b99c80b4
seokuuu <114736988+seokuuu@users.noreply.github.com>
2022-11-21 14:54:08 +0900

    오류 수정...

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -3,6 +3,10 @@ import { registerSlice } from "../modules/registerSlice";
 import { mypageSlice } from "../modules/mypageSlice";
 import calendarList from "../modules/calendarSlice";
 import comment from "../modules/commentSlice";
+<<<<<<< HEAD
+=======
+import courses from "../modules/addCreateSlice";
+>>>>>>> 8d71fbb (오류 수정...)
 import { boardSlice } from "../modules/boardSlice";
 
 export const store = configureStore({
@@ -12,5 +16,9 @@ export const store = configureStore({
     mypage: mypageSlice.reducer,
     calendarList,
     comment,
+<<<<<<< HEAD
+=======
+    courses,
+>>>>>>> 8d71fbb (오류 수정...)
   },
 });
