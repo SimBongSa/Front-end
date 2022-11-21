91c5a4e154908490ee4b09b8f79988ea304577ee
seokuuu <114736988+seokuuu@users.noreply.github.com>
2022-11-21 13:13:56 +0900

    오류 수정

--- src/redux/modules/boardSlice.js
+++ src/redux/modules/boardSlice.js
@@ -12,9 +12,9 @@ export const __createBoard = createAsyncThunk(
       formData.append(key, value);
     });
 
-    //formData console.log
+    //formData console.logc
     // for (let key of formData.keys()) {
-    //   console.log("formData ===>", key, ":", formData.get(key));
+    //   console.log("formData ==>", key, ":", formData.get(key));
     // }
 
     try {
@@ -46,10 +46,6 @@ export const __getBoard = createAsyncThunk(
 export const __getBoardId = createAsyncThunk(
   "getBoardsId",
   async (payload, thunkAPI) => {
-<<<<<<< HEAD
-    console.log("__getBoardsId => ", payload);
-=======
->>>>>>> 3ebd3b932d6a72c9c1a47d6717f040bfed8042fb
     try {
       const response = await apis.getBoardId(payload);
       return thunkAPI.fulfillWithValue(response.data.data);
@@ -104,29 +100,22 @@ export const __getArea = createAsyncThunk(
   async (payload, thunkAPI) => {
     try {
       const response = await payload;
-<<<<<<< HEAD
-      console.log(payload);
-      console.log(response);
-=======
->>>>>>> 3ebd3b932d6a72c9c1a47d6717f040bfed8042fb
       return thunkAPI.fulfillWithValue(response);
     } catch (error) {
       return thunkAPI.rejectWithValue(error);
     }
   }
 );
-<<<<<<< HEAD
-=======
 
 export const __postApply = createAsyncThunk(
   "apply",
   async (payload, thunkAPI) => {
-    console.log(payload)
+    console.log(payload);
     try {
-      const response = await apis.applyBoard(payload)
+      const response = await apis.applyBoard(payload);
       if (response.status === 200) {
-        alert(response.data.data.msg)
-        console.log(response.data.data.msg)
+        alert(response.data.data.msg);
+        console.log(response.data.data.msg);
         return thunkAPI.fulfillWithValue(response.data.data.msg);
       }
     } catch (error) {
@@ -134,7 +123,6 @@ export const __postApply = createAsyncThunk(
     }
   }
 );
->>>>>>> 3ebd3b932d6a72c9c1a47d6717f040bfed8042fb
 
 export const boardSlice = createSlice({
   name: "boards",
@@ -149,14 +137,9 @@ export const boardSlice = createSlice({
   reducers: {},
   extraReducers: (builder) => {
     builder
-<<<<<<< HEAD
+
       // POST (__createBoard)
       .addCase(__createBoard.pending, (state, _) => {
-=======
-
-      // boards get
-      .addCase(__getBoards.pending, (state, _) => {
->>>>>>> 3ebd3b932d6a72c9c1a47d6717f040bfed8042fb
         state.isLoading = true;
       })
       .addCase(__createBoard.fulfilled, (state, action) => {
@@ -169,13 +152,8 @@ export const boardSlice = createSlice({
         state.error = action.payload;
       })
 
-<<<<<<< HEAD
       //Total GET (__getBoard)
       .addCase(__getBoard.pending, (state, _) => {
-=======
-      // detail board get
-      .addCase(__getBoardsId.pending, (state, _) => {
->>>>>>> 3ebd3b932d6a72c9c1a47d6717f040bfed8042fb
         state.isLoading = true;
       })
       .addCase(__getBoard.fulfilled, (state, action) => {
@@ -187,7 +165,6 @@ export const boardSlice = createSlice({
         state.error = action.payload;
       })
 
-<<<<<<< HEAD
       //Each GET (__getBoardId)
       .addCase(__getBoardId.pending, (state, _) => {
         state.isLoading = true;
@@ -202,15 +179,11 @@ export const boardSlice = createSlice({
       })
 
       //area GET
-=======
-      // 지역 정보 get
->>>>>>> 3ebd3b932d6a72c9c1a47d6717f040bfed8042fb
       .addCase(__getArea.fulfilled, (state, action) => {
         state.isLoading = false;
         state.area = action.payload;
       })
 
-<<<<<<< HEAD
       //PUT (__editCreate)
       .addCase(__editBoard.pending, (state, _) => {
         state.isLoading = true;
@@ -239,15 +212,14 @@ export const boardSlice = createSlice({
       .addCase(__delBoard.rejected, (state, action) => {
         state.isLoading = false;
         state.error = action.payload;
-      });
-=======
+      })
+
       // 봉사 신청 post
       .addCase(__postApply.fulfilled, (state, action) => {
         state.isLoading = false;
-        console.log()
+        console.log();
         state.apply = action.payload;
-      })
->>>>>>> 3ebd3b932d6a72c9c1a47d6717f040bfed8042fb
+      });
   },
 });
 
