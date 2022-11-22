9eefac3a6dd93b1343e51e8ddf6a77f1e1d980fb
seokuuu <114736988+seokuuu@users.noreply.github.com>
2022-11-23 03:32:04 +0900

    header.jsx 오타 수정!

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,12 @@
 import { useNavigate } from "react-router-dom";
 import {
-  HeaderContainer,
-  HeaderLogo,
-  HeaderMenu,
-  HeaderMenuItem,
-  HeaderRegister,
-  LightThemeBtn,
-  UserIcon,
+	HeaderContainer,
+	HeaderLogo,
+	HeaderMenu,
+	HeaderMenuItem,
+	HeaderRegister,
+	LightThemeBtn,
+	UserIcon,
 } from "./Header.styled";
 import { BsFillMoonFill } from "react-icons/bs";
 import { useTheme } from "../../context/themeProvider";
@@ -16,62 +16,65 @@ import { useCookies } from "react-cookie";
 import SearchBar from "../SearchBar/SearchBar";
 
 const Header = () => {
-  const navigate = useNavigate();
-  const [ThemeMode, toggleTheme] = useTheme();
-  const [cookies] = useCookies(["Authorization"]);
+	const navigate = useNavigate();
+	const [ThemeMode, toggleTheme] = useTheme();
+	const [cookies] = useCookies(["Authorization"]);
 
-  useEffect(() => {
-    getCookieToken();
-  });
-A
-  const isLogin = cookies["access-token"];
-  const authority = cookies["authority"];
-  const username = cookies["username"];
+	useEffect(() => {
+		getCookieToken();
+	});
+	const isLogin = cookies["access-token"];
+	const authority = cookies["authority"];
+	const username = cookies["username"];
 
-  return (
-    <HeaderContainer>
-      <SearchBar/>
-      <HeaderLogo onClick={() => navigate("/")}>VONGOLE</HeaderLogo>
-      <LightThemeBtn onClick={toggleTheme}>
-        <BsFillMoonFill>Theme</BsFillMoonFill>
-      </LightThemeBtn>
-      <HeaderMenu>
-        <HeaderMenuItem onClick={() => navigate("/boards")}>Boards</HeaderMenuItem>
-        {isLogin && authority === "ROLE_MEMBER" ? (
-          <>
-            <HeaderMenuItem>Notice</HeaderMenuItem>
-            <HeaderMenuItem>Messagse</HeaderMenuItem>
-            <UserIcon
-              onClick={() => {
-                navigate("/usermypage");
-              }}
-            />
-          </>
-        ) : isLogin && authority === "ROLE_ADMIN" ? (
-          <>
-            <HeaderMenuItem>Notice</HeaderMenuItem>
-            <HeaderMenuItem>Messagse</HeaderMenuItem>
-            <HeaderMenuItem onClick={() => {
-              navigate("/recruit")
-            }}>봉사활동 등록하기</HeaderMenuItem>
-            <UserIcon
-              onClick={() => {
-                navigate("/companypage");
-              }}
-            />
-          </>
-        ) : (
-          <HeaderRegister
-            onClick={() => {
-              navigate("/login");
-            }}
-          >
-            Login
-          </HeaderRegister>
-        )}
-      </HeaderMenu>
-    </HeaderContainer>
-  );
+	return (
+		<HeaderContainer>
+			<SearchBar />
+			<HeaderLogo onClick={() => navigate("/")}>VONGOLE</HeaderLogo>
+			<LightThemeBtn onClick={toggleTheme}>
+				<BsFillMoonFill>Theme</BsFillMoonFill>
+			</LightThemeBtn>
+			<HeaderMenu>
+				<HeaderMenuItem onClick={() => navigate("/boards")}>Boards</HeaderMenuItem>
+				{isLogin && authority === "ROLE_MEMBER" ? (
+					<>
+						<HeaderMenuItem>Notice</HeaderMenuItem>
+						<HeaderMenuItem>Messagse</HeaderMenuItem>
+						<UserIcon
+							onClick={() => {
+								navigate("/usermypage");
+							}}
+						/>
+					</>
+				) : isLogin && authority === "ROLE_ADMIN" ? (
+					<>
+						<HeaderMenuItem>Notice</HeaderMenuItem>
+						<HeaderMenuItem>Messagse</HeaderMenuItem>
+						<HeaderMenuItem
+							onClick={() => {
+								navigate("/recruit");
+							}}
+						>
+							봉사활동 등록하기
+						</HeaderMenuItem>
+						<UserIcon
+							onClick={() => {
+								navigate("/companypage");
+							}}
+						/>
+					</>
+				) : (
+					<HeaderRegister
+						onClick={() => {
+							navigate("/login");
+						}}
+					>
+						Login
+					</HeaderRegister>
+				)}
+			</HeaderMenu>
+		</HeaderContainer>
+	);
 };
 
 export default Header;
