e66e8d594a782ef30ba6a53b95d4eb754e5b4079
seokuuu <114736988+seokuuu@users.noreply.github.com>
2022-12-01 12:47:40 +0900

    recruit 페이지 수정중

--- src/components/Recruit/Recruit.jsx
+++ src/components/Recruit/Recruit.jsx
@@ -2,11 +2,12 @@ import { useState } from "react";
 import { useDispatch } from "react-redux";
 import PopupDom from "../Map/PopupDom";
 import PopupPostCode from "../Map/PopupPostCode";
-import Input from "../common/input/Input";
+// import InPut from "../common/input/Input";
 import { __createBoard } from "../../redux/modules/boardSlice";
 import "react-datepicker/dist/react-datepicker.css";
 import moment from "moment";
 import { ko } from "date-fns/esm/locale";
+import styled from "styled-components";
 import {
 	RecruitContainer,
 	RecruitNav,
@@ -27,7 +28,8 @@ const Recruit = () => {
 	// date picker
 	const today = new Date();
 	const [startDate, setStartDate] = useState(today);
-	const [endDate, setEndDate] = useState(today);
+	const [endDate, setEndDate] = useState(null);
+	const [dueDay, setDueDay] = useState(new Date());
 
 	// 팝업창 열기
 	const openPostCode = () => {
@@ -38,6 +40,18 @@ const Recruit = () => {
 	const closePostCode = () => {
 		setIsPopupOpen(false);
 	};
+
+	const onChange = dates => {
+		const [start, end, due] = dates;
+		setStartDate(start);
+		setEndDate(end);
+		setDueDay(due);
+	};
+
+	// const onChangeDue = day => {
+	// 	const [due] = day;
+	// 	setDueDay(due);
+	// };
 
 	const init = {
 		title: "",
@@ -97,6 +111,7 @@ const Recruit = () => {
 				boardImage,
 				startDate: moment(startDate).format("YYYY-MM-DD"),
 				endDate: moment(endDate).format("YYYY-MM-DD"),
+				dueDay: moment(dueDay).format("YYYY-MM-DD"),
 			})
 		);
 	};
@@ -104,55 +119,20 @@ const Recruit = () => {
 	return (
 		<div data-spy="scroll" data-target="#navbar">
 			<RecruitContainer>
-				<RecruitNav id="navbar">
-					<ul className="nav">
-						<li role="presentation" className="active">
-							<a href="#section1">
-								<span className="counter">01</span>
-								<h3 className="title">Intro</h3>
-								<p className="body">봉사활동 이름이랑 이것 저것 적으셈</p>
-							</a>
-						</li>
-
-						<li role="presentation">
-							<a href="#section2">
-								<span className="counter">02</span>
-								<h3 className="title">Section 02</h3>
-								<p className="body">봉사활동 이름이랑 이것 저것 적으셈</p>
-							</a>
-						</li>
-
-						<li role="presentation">
-							<a href="#section3">
-								<span className="counter">03</span>
-								<h3 className="title">Section 03</h3>
-								<p className="body">봉사활동 이름이랑 이것 저것 적으셈</p>
-							</a>
-						</li>
-
-						<li role="presentation">
-							<a href="#section4">
-								<span className="counter">04</span>
-								<h3 className="title">Section 04</h3>
-								<p className="body">봉사활동 이름이랑 이것 저것 적으셈</p>
-							</a>
-						</li>
-					</ul>
-					<ScrollDown />
-				</RecruitNav>
-				<form onSubmit={submitHandler}>
+				{/* <form onSubmit={submitHandler}>
 					<RecruitSec className="section section1" id="section1">
-						<h1>어떤 봉사활동을 등록하시나요?</h1>
+						<h1>봉사 등록하기</h1>
+						<h1>봉사 활동에 대해 궁금해요!</h1>
 						<Tags category={true} onChangeTags={onChangeTags} />
 						<Input
-							placeholder="제목"
+							placeholder="봉사 활동 주제"
 							type="text"
 							name="title"
 							value={input.title}
 							onChange={e => onChangeInput(e)}
 						/>
 						<RecruitTA
-							placeholder="봉사 활동을 설명해주세요"
+							placeholder="봉사 활동 내용"
 							type="text"
 							name="content"
 							value={input.content}
@@ -167,28 +147,18 @@ const Recruit = () => {
 								locale={ko}
 								dateFormat="📅 yyyy년-MM월-dd일"
 								selected={startDate}
-								onChange={date => setStartDate(date)}
-								selectsStart
+								onChange={onChange}
 								startDate={startDate}
 								endDate={endDate}
+								selectsRange
 							/>
 							<CustomeDatePicker
 								locale={ko}
 								dateFormat="📅 yyyy년-MM월-dd일 "
-								selected={endDate}
-								onChange={date => setEndDate(date)}
-								selectsEnd
-								startDate={startDate}
-								endDate={endDate}
-								minDate={startDate}
+								selected={dueDay}
+								onChange={date => setDueDay(date)}
 							/>
 						</PickerBox>
-						<Input
-							type="date"
-							name="dueDay"
-							value={input.dueDay}
-							onChange={e => onChangeInput(e)}
-						/>
 						<Input
 							placeholder="행사 장소(우편번호 검색 클릭)"
 							type="text"
@@ -232,10 +202,100 @@ const Recruit = () => {
 						/>
 						<button>봉사활동 등록하기</button>
 					</RecruitSec>
-				</form>
+				</form> */}
+
+				<Layout>
+					<h2>봉사 등록하기</h2>
+					<FormBox>
+						<form action="submit">
+							<div class="leftBox">
+								<h3>
+									<span>봉사 활동</span>에 대해 궁금해요!
+								</h3>
+								<Input type="text" placeholder="내용" />
+								<Input type="text" placeholder="카테고리" />
+								<Input type="text" placeholder="봉사 기간" />
+								<Input type="text" placeholder="봉사 장소" />
+								<TextArea name="" id="" cols="30" rows="10"></TextArea>
+								<Input type="text" placeholder="이미지" />
+							</div>
+							<div class="rightBox">
+								<h3>
+									<span>모집 활동</span>궁금해요!
+								</h3>
+								<Input type="text" placeholder="기간" />
+								<Input type="text" placeholder="필수" />
+								<div class="tag">
+									<p>
+										<span>#19세</span> <span>#남성</span>
+									</p>
+								</div>
+								<button>등록하기</button>
+							</div>
+							<div></div>
+						</form>
+					</FormBox>
+				</Layout>
 			</RecruitContainer>
 		</div>
 	);
 };
 
 export default Recruit;
+
+const Layout = styled.div`
+	width: 1440px;
+	margin: 0 auto;
+	& h2 {
+		color: #333;
+		z-index: 1000;
+	}
+`;
+
+const FormBox = styled.form`
+	& form {
+		width: 100%;
+		display: flex;
+		justify-content: space-between;
+		& div {
+			display: flex;
+			flex-direction: column;
+			& h3 {
+				& span {
+					color: #66885d;
+				}
+			}
+		}
+	}
+`;
+
+const Input = styled.input`
+	display: block;
+	width: 590px;
+	height: 60px;
+	border-radius: 30px;
+	/* background-image: url(); */
+	background-position: center right 10px;
+	background-repeat: no-repeat;
+	margin-bottom: 10px;
+	border: 1px solid #66885d;
+	padding-left: 10px;
+`;
+
+const TextArea = styled.textarea`
+	border: 1px solid #66885d;
+	resize: none;
+	textarea:focus {
+		outline: none;
+	}
+	input:focus {
+		outline: none;
+	}
+	button {
+		width: 300px;
+		height: 60px;
+		border-radius: 30px;
+		background-color: #66885d;
+		border: none;
+	}
+`;
