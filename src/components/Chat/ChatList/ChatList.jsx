73c31dbfd224f0e863c897d6fc4eb50a1aa8769a
seokuuu <114736988+seokuuu@users.noreply.github.com>
2022-12-13 16:34:10 +0900

    비회원 로그인시 신청/채팅 기능 취소

--- src/components/Chat/ChatList/ChatList.jsx
+++ src/components/Chat/ChatList/ChatList.jsx
@@ -1,58 +1,52 @@
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { __getBoardId } from "../../../redux/modules/boardSlice"
+import { __getBoardId } from "../../../redux/modules/boardSlice";
 import styled from "styled-components";
 import { StSidePanel, StContact, StContactWrap } from "./ChatList.styled";
 
 export const ChatList = () => {
+	const navigate = useNavigate();
+	const dispatch = useDispatch();
+	const chatList = useSelector(state => state?.chat?.chatList);
 
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-  const chatList = useSelector((state) => state?.chat?.chatList);
-  console.log(chatList);
-
-  return (
-    <StSidePanel>
-      <h1>문의 내역</h1>
-      <StContact>
-        <ul>
-        {
-          chatList.length === 0 ? 
-            <div onClick={() => {
-              navigate('/boards')
-            }}>
-              <h4>문의 내역이 없어요!</h4>
-              <h4>게시물 조회하기</h4>
-            </div>
-           : (
-            chatList?.map((item) => {
-              return (
-                <StListGroup 
-                  key={item.chatRoomId}
-                  onClick={() => {
-                    navigate(`/chat/${item.chatRoomId}`);
-                    dispatch(__getBoardId(item.boardId)); 
-                  }}
-                >
-                  <StContactWrap>{item.roomName}</StContactWrap>
-                  <StContactWrap>{item.userNameList.split(' ')[1]}</StContactWrap>
-                </StListGroup>
-              )
-            })
-            )
-          }
-        </ul>
-      </StContact>
-    </StSidePanel>
-  )
-}
+	return (
+		<StSidePanel>
+			<h1>문의 내역</h1>
+			<StContact>
+				<ul>
+					{chatList.length === 0 ? (
+						<div
+							onClick={() => {
+								navigate("/boards");
+							}}
+						>
+							<h4>문의 내역이 없어요!</h4>
+							<h4>게시물 조회하기</h4>
+						</div>
+					) : (
+						chatList?.map(item => {
+							return (
+								<StListGroup
+									key={item.chatRoomId}
+									onClick={() => {
+										navigate(`/chat/${item.chatRoomId}`);
+										dispatch(__getBoardId(item.boardId));
+									}}
+								>
+									<StContactWrap>{item.roomName}</StContactWrap>
+								</StListGroup>
+							);
+						})
+					)}
+				</ul>
+			</StContact>
+		</StSidePanel>
+	);
+};
 
 export default ChatList;
 
 export const StListGroup = styled.li`
-  margin: auto 15px;
-  padding: 0;
-  &:nth-child(1) {
-    color: ${(props) => props.theme.btnColor};
-  }
-`
+	margin: auto 15px;
+	padding: 0;
+`;
